5d750148802ef0368468ec8362b22a1fbbe8131c
Raffael Christopher <raffaelcss@hotmail.com>
2024-10-02 15:23:53 -0300

    bg color white

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MapWithGeocoder from "../components/searchMap/page";
 
 export default function Home() {
   return (
-    <div className="flex bg-slate-100 justify-center items-center w-full md:h-screen">
+    <div className="flex justify-center items-center w-full md:h-screen">
       <div className="p-6 rounded-md flex flex-col md:flex-row items-center gap-3">
         <InfoContainer />
         <div className="h-[15rem] md:h-[30rem] w-[22rem] md:w-[40rem] bg-red-500 rounded-lg overflow-hidden">
